60ceaf7901d6f0eeb2d6d13edbe4100e6fab640a
Hiroyuki Sano <sh19910711@gmail.com>
2014-10-18 17:14:47 +0900

    fix get_contest_line()

--- lib/server/common.rb
+++ lib/server/common.rb
@@ -51,9 +51,13 @@ module Server
   # はてなグループのカレンダー用のテキストを取得する
   def get_contest_line(contest)
     date     = contest["date"]
-    str_date = date.strftime("%H:%M")
     title    = contest["title"]
     tag      = contest["tag"]
-    title.include?(tag) ?  "* #{str_date} #{title}" : "* #{str_date} [#{tag}] #{title}"
+    if contest["is_date"]
+      str_date = ""
+    else
+      str_date = "#{date.strftime("%H:%M")} "
+    end
+    title.include?(tag) ?  "* #{str_date}#{title}" : "* #{str_date}[#{tag}] #{title}"
   end
 end
